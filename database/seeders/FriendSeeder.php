f7e0b394c8a50a627b89871b8aaee391b9b55474
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-10-19 11:49:23 -0400

    Add FriendSeeder.php

--- database/seeders/FriendSeeder.php
+++ database/seeders/FriendSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Profiles;
+use Illuminate\Support\Facades\DB;
 
 class FriendSeeder extends Seeder
 {
@@ -14,6 +16,32 @@ class FriendSeeder extends Seeder
      */
     public function run()
     {
-        
+        $profiles = Profiles::all();
+        $numberOfProfiles = $profiles->count();
+
+        foreach ($profiles as $profile) {
+            $numberOfFriends = rand(0, $numberOfProfiles);
+
+            for ($i = 0; $i <= $numberOfFriends; $i++) {
+                $friendId = rand(1, $numberOfProfiles);
+
+                if ($profile->id !== $friendId) {
+
+                    if (!DB::table('friends')->where('profile_id', $profile->id)->where('friend_id', $friendId)->count()) {
+                        DB::table('friends')->insert([
+                            'profile_id' => $profile->id,
+                            'friend_id' => $friendId
+                        ]);
+                    }
+
+                    if (!DB::table('friends')->where('profile_id', $friendId)->where('friend_id', $profile->id)->count()) {
+                        DB::table('friends')->insert([
+                            'profile_id' => $friendId,
+                            'friend_id' => $profile->id
+                        ]);
+                    }
+                }
+            }
+        }
     }
 }
