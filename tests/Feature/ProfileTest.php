927291464dcd65ddd3d9ed68bb92aa996e6f4c90
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-10-19 13:28:13 -0400

    Add canCreateAPost test

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -2,12 +2,16 @@
 
 namespace Tests\Feature;
 
+use App\Models\Profile;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ProfileTest extends TestCase
 {
+
+    use RefreshDatabase;
+
     /**
      * A basic feature test example.
      *
@@ -19,4 +23,47 @@ class ProfileTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    /** @test*/
+    public function canCreateAProfile()
+    {
+        $this->withoutExceptionHandling();
+
+        $data = [
+            'id'=> '100',
+            'img' => 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinys
+            rgb&dpr=1&w=500',
+            'first_name' => 'Steph',
+            'last_name' => 'Walters',
+            'phone' => '(820) 289-1818',
+            'address' => '5190 Center Court Drive',
+            'city' => 'Spring',
+            'state' => 'TX',
+            'zipcode' => '77370',
+            'available' => 'true',
+        ];
+
+        $response = $this->json('POST', '/api/profile', $data);
+
+        $response->assertOk();
+        $this->assertCount(1, Profile::all());
+
+        /* $response->assertStatus(200)
+             ->assertJson(compact('data'));
+
+        $this->assertDatabaseHas('profiles', [
+          'id' => $data['id'],
+          'img' => $data['img'],
+          'first_name' => $data['first_name'],
+          'last_name' => $data['last_name'],
+          'phone' => $data['phone'],
+          'address' => $data['address'],
+          'city' => $data['city'],
+          'state' => $data['state'],
+          'zipcode' =>$data['zipcode'],
+          'available' => $data['available'],
+        ]); */
+    }
+
+    
 }
