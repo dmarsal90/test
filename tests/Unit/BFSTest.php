9833b12697fe7162a4bf68eb7f30031ef34ea92c
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-10-21 14:51:20 -0400

    Update testBFS

--- tests/Unit/BFSTest.php
+++ tests/Unit/BFSTest.php
@@ -16,17 +16,21 @@ class BFSTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
     public function testBFS()
     {
         $bfs =  $this->action('GET','ProfileController@getShortestPath');
+
         $Graph = [
             'A' => ['B', 'C'],
             'B' => ['A', 'D'],
-            'D' => ['B'],
-            'C' => ['A',],
+            'D' => ['B', 'E'],
+            'C' => ['A', 'E'],
+            'E' => ['C', 'B', 'D'],
         ];
 
-        $this->assertTrue($bfs($Graph, 'A', 'D'));
+        $this->assertEquals($bfs($Graph, 'A', 'D'), ['A', 'B', 'D']);
+        $this->assertEquals($bfs($Graph, 'A', 'E'), ['A', 'C', 'E']);
         $this->assertFalse($bfs($Graph, 'A', 'F'));
     }
 }
