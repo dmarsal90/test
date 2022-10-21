644d464d4936644cad61b0824dd8f9b6ca0c8d45
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-10-21 14:47:43 -0400

    Add testBFS

--- tests/Unit/BFSTest.php
+++ tests/Unit/BFSTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+
 use PHPUnit\Framework\TestCase;
 
 class BFSTest extends TestCase
@@ -14,5 +15,18 @@ class BFSTest extends TestCase
     public function test_example()
     {
         $this->assertTrue(true);
+    }
+    public function testBFS()
+    {
+        $bfs =  $this->action('GET','ProfileController@getShortestPath');
+        $Graph = [
+            'A' => ['B', 'C'],
+            'B' => ['A', 'D'],
+            'D' => ['B'],
+            'C' => ['A',],
+        ];
+
+        $this->assertTrue($bfs($Graph, 'A', 'D'));
+        $this->assertFalse($bfs($Graph, 'A', 'F'));
     }
 }
